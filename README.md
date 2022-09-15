e22e8aef9cdd80ef25ad7d05a19ade4ea4276591
hughding <849308919@qq.com>
2022-09-15 15:00:19 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cmake_demo
+环境配置
+MinGW安装，bin加入到Path中
+cmake安装，使用Clion中的cmake，bin加入到Path中
+
+
+git submodule update --init --recursive
+
+find_path 搜索指定头文件路径。
+find_library 搜索指定动态文件路径。
---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "sub.h"
+#include "sub2.h"
+#include "gtest/gtest.h"
+
+int main() {
+    sub::TestClass* tc;
+    tc = new sub::TestClass(1, "test hello world");
+    std::cout << "test " + tc->toString() << std::endl;
+    delete tc;
+    sub2::print("test abc");
+}
